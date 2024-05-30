68367719148ea4c470782c1c150542972bd191c1
Mikalai Borakhau <nickolaib@sam-solutions.net>
2024-05-30 16:48:00 +0300

    Update 3

--- src/main/java/com/itacademy/utils/retry/AnnotationTransformer.java
+++ src/main/java/com/itacademy/utils/retry/AnnotationTransformer.java
@@ -1,11 +1,12 @@
 package com.itacademy.utils.retry;
 
+import org.testng.IAnnotationTransformer;
 import org.testng.annotations.ITestAnnotation;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
-public class AnnotationTransformer {
+public class AnnotationTransformer implements IAnnotationTransformer {
 
     @Override
     public void transform(ITestAnnotation annotation,
